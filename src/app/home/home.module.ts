3b2b38eb15b9636588b64f9ce9907fe38a709390
Dawlin Pena Luciano <dawmusic19@gmail.com>
2018-08-10 08:31:45 +0100

    Removing more wrong references

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,6 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { routes } from './home.routes';
-import { PokemonComponent } from '../pokemon/pokemon.component';
 import { ChildDetailComponent } from '../pokemon/child-detail/child-detail.component';
 
 @NgModule({
@@ -11,7 +10,6 @@ import { ChildDetailComponent } from '../pokemon/child-detail/child-detail.compo
     /**
      * Components / Directives/ Pipes
      */
-    PokemonComponent,
     ChildDetailComponent
   ],
   imports: [
