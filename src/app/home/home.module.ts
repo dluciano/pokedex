d82421da2be18eb636af4e858a5f2bdede423525
Dawlin Pena Luciano <dawmusic19@gmail.com>
2018-08-10 08:39:05 +0100

    Other more wrong references

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,15 +2,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
-import { routes } from './home.routes';
-import { ChildDetailComponent } from '../pokemon/child-detail/child-detail.component';
-
 @NgModule({
   declarations: [
     /**
      * Components / Directives/ Pipes
      */
-    ChildDetailComponent
   ],
   imports: [
     CommonModule,
@@ -18,5 +14,4 @@ import { ChildDetailComponent } from '../pokemon/child-detail/child-detail.compo
   ],
 })
 export class HomeModule {
-  public static routes = routes;
 }
