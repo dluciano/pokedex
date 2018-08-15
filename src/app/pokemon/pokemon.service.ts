8d7eba6d370d1daa8286069a83e1ec79a19c3b7b
Dawlin Pena Luciano <dawmusic19@gmail.com>
2018-08-15 14:35:43 +0100

    Integrated the Pokeapi-js-wrapper lib

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Pokemon } from './pokemon.model';
 import { PokemonList } from './PokemonList';
+import { Pokedex } from 'pokeapi-js-wrapper';
 
 export interface IApiService<T> {
   getByUrl(entity: T): Promise<T>;
@@ -9,8 +10,17 @@ export interface IApiService<T> {
   getById(id: number): Promise<T>;
 }
 
+const P = new Pokedex({
+  protocol: 'http',
+  hostName: 'pokeapi.salestock.net/',
+  versionPath: '/api/v2/',
+  cache: true,
+  timeout: 5 * 1000 // 5s
+});
+
 @Injectable()
 export class PokemonService implements IApiService<Pokemon> {
+
   private cache: PokemonList;
   private endpoint: string = 'http://pokeapi.salestock.net/api/v2';
   private resources = {
@@ -38,19 +48,15 @@ export class PokemonService implements IApiService<Pokemon> {
 
   public getAll(limit: number, offset: number): Promise<PokemonList> {
     // First, get all the urls of the pokemons, using as paginator the limit and offset variable.
-    return this
-      .http
-      .get(this.endpoint + this.resources.pokemon + '?limit=' + limit + '&offset=' + offset)
-      .toPromise()
-      .then((response: any) => {
-        const pokemons = response.results.map((p: any) => {
-          return Pokemon.initial(p.name, p.url);
-        });
-        const list = new PokemonList(response.count, response.previous, response.next);
-        pokemons.forEach((p) => list.push(p));
-        this.cache = list;
-        return list;
+    return P.getPokemonsList({ limit, offset }).then((response: any) => {
+      const pokemons = response.results.map((p: any) => {
+        return Pokemon.initial(p.name, p.url);
       });
+      const list = new PokemonList(response.count, response.previous, response.next);
+      pokemons.forEach((p) => list.push(p));
+      // this.cache = list;
+      return list;
+    });
   }
 
   public getByUrl(entity: Pokemon): Promise<Pokemon> {
